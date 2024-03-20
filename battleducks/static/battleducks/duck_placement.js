d4cabfa26b998daefa6897a66facfa5c930f1380
amith-shetty <amithshettya@gmail.com>
2024-03-20 19:26:11 -0400

    Contributor: Amith Changes: Drag and drop shows shadows. - Green shadow is show to make drag and drop easier. - Blue shadow shows when the duck is placed. - Boundary condition to handle duck placement over flowing the board.

--- battleducks/static/battleducks/duck_placement.js
+++ battleducks/static/battleducks/duck_placement.js
@@ -1,93 +1,34 @@
-function setUpDuckPlacementBoard() {
-    const grid = document.getElementById(`battleship-grid`);
-
-    for (let i = 0; i < 15; i++) { // 15x15 grid
-        for (let j = 0; j < 15; j++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.id = `cell-${i}-${j}`; // Assigns a unique ID to each cell
-            grid.appendChild(cell);
-        }
-    }
+const duckSizeToArea = {
+    'normal': [
+        [0, 0],
+    ], 
+    'chubby': [
+        [0, 0], [0, 1],
+    ], 
+    'long': [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+    ], 
+    'wide': [
+        [0, 0], [0, 1],
+        [1, 0], [1, 1],
+        [2, 0], [2, 1],
+        [3, 0], [3, 1],
+    ], 
+    'very-long': [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+    ],
 }
 
-function setUpDucks() {
-    const duckSizes = ['normal', 'chubby', 'long', 'wide', 'very-long']
-    const ducks = document.getElementById(`ducks-container`);
-    
-    duckSizes.forEach(size => {
-        const duck = createDuck(size);
-
-        ducks.appendChild(duck);
-    });
-}
-
-function createDuck(size) {
-    const img = document.createElement('img');
-    img.src = '/static/battleducks/duck.png';
-    img.classList.add("duck");
-    img.classList.add(size);
-    img.dataset.key = size;
-
-    img.setAttribute('draggable', true);
-    return img
-}
-
-function setupDragAndDrop() {
-    const ducksContainer = document.getElementById('ducks-container');
-    const ducks = Array.from(ducksContainer.children);
-
-    ducks.forEach(function (duck){
-        duck.addEventListener('dragstart', handleDuckDragStart);
-        // duck.addEventListener('dragend', handleDuckDragEnd);
-       
-    })
-
-    const battleshipGrid = document.getElementById('battleship-grid');
-    const duckCells = Array.from(battleshipGrid.children);
-    
-    duckCells.forEach(function (cell) {
-        // cell.addEventListener('dragenter', handleDuckDragEnter);
-        cell.addEventListener('dragover', handleDuckDragOver);
-        // cell.addEventListener('dragleave', handleDuckDragLeave);
-        cell.addEventListener('drop', handleDuckDrop);
-    })
-}
-
-function handleDuckDragStart(e) {
-    e.dataTransfer.setData('size', e.target.dataset.key);
-}
-
-function handleDuckDragOver(e) {
-    e.preventDefault(); // allow dropping
-}
-
-function handleDuckDrop(e) {
-    e.preventDefault();
-    var size = e.dataTransfer.getData("size");
-    const duck = createDuck(size);
-
-    e.target.appendChild(duck);
-}
-
-
-
-
-
-// function handleDuckDragEnter(e) {
-//     this.classList.add('placing');
-//     e.dataTransfer.setData('duckPosition', this);
-// }
-
-// function handleDuckDragLeave(e) {
-//     this.classList.remove('placing');
-// }
-
-// function handleDuckDragEnd(e) {
-//     this.style.opacity = '1';
-// }
-
-
+// global variable, lets remove this later
+var sizeDuckBeingMoved = ""
 
 function setUpDuckPlacement() {
     setUpDuckPlacementBoard();
@@ -95,3 +36,144 @@ function setUpDuckPlacement() {
     setupDragAndDrop();
 }
 
+function setUpDuckPlacementBoard() {
+    const grid = document.getElementById(`battleship-grid`);
+
+    for (let y = 0; y < 15; y++) { // 15x15 grid
+        for (let x = 0; x < 15; x++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.id = `cell-${y}-${x}`; // Assigns a unique ID to each cell
+            grid.appendChild(cell);
+        }
+    }
+}
+
+function setUpDucks() {
+    const duckSizes = Object.keys(duckSizeToArea);
+    const ducks = document.getElementById(`ducks-container`);
+    
+    duckSizes.forEach(size => {
+        const duck = createDuck(size, true);
+        ducks.appendChild(duck);
+    });
+}
+
+function createDuck(size, draggable) {
+    const img = document.createElement('img');
+    img.src = '/static/battleducks/duck.png';
+    img.classList.add("duck");
+    img.classList.add(size);
+    img.dataset.size = size;
+
+    img.setAttribute('draggable', draggable);
+    return img
+}
+
+function setupDragAndDrop() {
+    const ducksContainer = document.getElementById('ducks-container');
+    const ducks = Array.from(ducksContainer.children);
+    ducks.forEach(function (duck){
+        duck.addEventListener('dragstart', recordDuckSize);
+    })
+
+    const battleshipGrid = document.getElementById('battleship-grid');
+    const duckCells = Array.from(battleshipGrid.children);
+
+    duckCells.forEach(function (cell) {
+        cell.addEventListener('dragover', showDuckShadow);
+        cell.addEventListener('drop', dropTheDuck);
+        cell.addEventListener('dragend', clearDuckShadow);
+    })
+}
+
+function recordDuckSize(e) {
+    sizeDuckBeingMoved = e.target.dataset.size;
+}
+
+function showDuckShadow(e) {
+    // By default, dropping is disabled.
+    e.preventDefault(); 
+
+    clearDuckShadow();
+
+    const duckSize = sizeDuckBeingMoved;
+    const cell_id = e.target.id;
+    const cellLocY = cell_id.split('-')[1]
+    const cellLocX = cell_id.split('-')[2]
+    showShadow(cellLocY, cellLocX, duckSize, "duck-shadow");
+}
+
+function dropTheDuck(e) {
+    var duckSize = sizeDuckBeingMoved;
+    const img = createDuck(duckSize, false);
+    img.style.position = 'absolute';
+    var cellLocY = e.target.getBoundingClientRect().top
+    var cellLocX = e.target.getBoundingClientRect().left
+    img.style.left = `${cellLocX}px`;
+    img.style.top = `${cellLocY}px`;
+
+    // Append the duck to the battleship grid
+    const battleshipGrid = document.getElementById('battleship-grid');
+    battleshipGrid.appendChild(img);
+
+    // add placement shadow
+    clearDuckShadow();
+    duckSize = sizeDuckBeingMoved;
+    const cell_id = e.target.id;
+    cellLocY = cell_id.split('-')[1]
+    cellLocX = cell_id.split('-')[2]
+    showShadow(cellLocY, cellLocX, duckSize, "duck-placed");
+}
+
+function clearDuckShadow() {
+    for (let y = 0; y < 15; y++) { // 15x15 grid
+        for (let x = 0; x < 15; x++) {
+            const id = `cell-${y}-${x}`;
+            const cell = document.getElementById(id);
+            if(cell.classList.contains("duck-shadow")){
+                cell.classList.remove("duck-shadow");
+            }
+        }
+    }
+}
+
+
+function showShadow(cellLocY, cellLocX, duckSize, shadowClass) {
+    areas = getDuckShadow(cellLocY, cellLocX, duckSize);
+    areas.forEach((coords) => {
+        const [y, x] = coords;
+        // if out of range clear shadow and exit
+        if(y<0 || y>= 15 || x<0 || x>=15) {
+            clearDuckShadow();
+            return;
+        }
+        
+        const id = `cell-${y}-${x}`;
+        const cell = document.getElementById(id);
+        if(cell === null) console.log(id);
+        cell.classList.add(shadowClass);
+    })
+}
+
+function getDuckShadow(refY, refX, size) {
+    const shadows = [];
+
+    const areas = duckSizeToArea[size];
+    areas.forEach((coords) => {
+        const [y, x] = coords;
+        const newY = parseInt(refY, 10) + parseInt(y, 10);
+        const newX = parseInt(refX, 10) + parseInt(x, 10);
+        shadows.push([newY,  newX]);
+    })
+    return shadows;
+}
+
+function notInBound(areas) {
+    areas.forEach((coords) => {
+        const [y, x] = coords;
+        if(y<0 || y>= 15 || x<0 || x>=15) return true;
+        console.log("evaluate", y, x)
+    })
+    return false;
+}

a3e8892ea24b088c308af513cfaba62a4f39edce
amith-shetty <amithshettya@gmail.com>
2024-03-31 11:07:03 -0400

    Contributor: Amith - Refactored duck placement   - Divided board and duck placement area   - Improved readability - Fixed bug  - Duck cannot be placed over another duck after the fix

--- battleducks/static/battleducks/duck_placement.js
+++ battleducks/static/battleducks/duck_placement.js
@@ -1,179 +1,243 @@
-const duckSizeToArea = {
-    'normal': [
-        [0, 0],
-    ], 
-    'chubby': [
-        [0, 0], [0, 1],
-    ], 
-    'long': [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-    ], 
-    'wide': [
-        [0, 0], [0, 1],
-        [1, 0], [1, 1],
-        [2, 0], [2, 1],
-        [3, 0], [3, 1],
-    ], 
-    'very-long': [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-        [4, 0],
-        [5, 0],
-    ],
+const BOARD_SIZE = 15;
+const DUCK_SIZES = [
+    'normal',
+    'chubby',
+    'long',
+    'wide',
+    'very-long',
+];
+DUCK_SPRITE = 'https://i.ibb.co/qxxPWsJ/duck.png';
+
+globalDraggedDuckSizeTracker = null;
+
+
+function createDucksFromDuckSizes(duckSizes) {
+    const ducks = [];
+    for(const size of duckSizes) {
+        ducks.push(new Duck(size));
+    }
+    return ducks;
 }
 
-// global variable, lets remove this later
-var sizeDuckBeingMoved = ""
-
-function setUpDuckPlacement() {
-    setUpDuckPlacementBoard();
-    setUpDucks();
-    setupDragAndDrop();
-}
-
-function setUpDuckPlacementBoard() {
-    const grid = document.getElementById(`battleship-grid`);
-
-    for (let y = 0; y < 15; y++) { // 15x15 grid
-        for (let x = 0; x < 15; x++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.id = `cell-${y}-${x}`; // Assigns a unique ID to each cell
-            grid.appendChild(cell);
+function getSizeFromImageElement(img) {
+    for (let size of DUCK_SIZES) {
+        if (img.classList.contains(size)) {
+            return size;
         }
+    }
+    return null;
+}
+
+function getCellIDFromCoordinate(x, y) {
+  return `cell-${y}-${x}`
+}
+
+function getCoordinateFromCellID(cell_id){
+  return {
+    x: parseInt(cell_id.split('-')[2], 10),
+    y: parseInt(cell_id.split('-')[1], 10),
+  }
+}
+
+
+class Duck {
+    constructor(size) {
+        this.element = document.createElement('img');
+        this.element.src = DUCK_SPRITE;
+        this.element.classList.add("duck");
+        this.element.classList.add(size);
+        this.element.setAttribute('draggable', true);
     }
 }
 
-function setUpDucks() {
-    const duckSizes = Object.keys(duckSizeToArea);
-    const ducks = document.getElementById(`ducks-container`);
-    
-    duckSizes.forEach(size => {
-        const duck = createDuck(size, true);
-        ducks.appendChild(duck);
-    });
-}
 
-function createDuck(size, draggable) {
-    const img = document.createElement('img');
-    img.src = '/static/battleducks/duck.png';
-    img.classList.add("duck");
-    img.classList.add(size);
-    img.dataset.size = size;
+class Board {
+    constructor(boardElement) {
+        this.element = boardElement;
+    }
 
-    img.setAttribute('draggable', draggable);
-    return img
-}
-
-function setupDragAndDrop() {
-    const ducksContainer = document.getElementById('ducks-container');
-    const ducks = Array.from(ducksContainer.children);
-    ducks.forEach(function (duck){
-        duck.addEventListener('dragstart', recordDuckSize);
-    })
-
-    const battleshipGrid = document.getElementById('battleship-grid');
-    const duckCells = Array.from(battleshipGrid.children);
-
-    duckCells.forEach(function (cell) {
-        cell.addEventListener('dragover', showDuckShadow);
-        cell.addEventListener('drop', dropTheDuck);
-        cell.addEventListener('dragend', clearDuckShadow);
-    })
-}
-
-function recordDuckSize(e) {
-    sizeDuckBeingMoved = e.target.dataset.size;
-}
-
-function showDuckShadow(e) {
-    // By default, dropping is disabled.
-    e.preventDefault(); 
-
-    clearDuckShadow();
-
-    const duckSize = sizeDuckBeingMoved;
-    const cell_id = e.target.id;
-    const cellLocY = cell_id.split('-')[1]
-    const cellLocX = cell_id.split('-')[2]
-    showShadow(cellLocY, cellLocX, duckSize, "duck-shadow");
-}
-
-function dropTheDuck(e) {
-    var duckSize = sizeDuckBeingMoved;
-    const img = createDuck(duckSize, false);
-    img.style.position = 'absolute';
-    var cellLocY = e.target.getBoundingClientRect().top
-    var cellLocX = e.target.getBoundingClientRect().left
-    img.style.left = `${cellLocX}px`;
-    img.style.top = `${cellLocY}px`;
-
-    // Append the duck to the battleship grid
-    const battleshipGrid = document.getElementById('battleship-grid');
-    battleshipGrid.appendChild(img);
-
-    // add placement shadow
-    clearDuckShadow();
-    duckSize = sizeDuckBeingMoved;
-    const cell_id = e.target.id;
-    cellLocY = cell_id.split('-')[1]
-    cellLocX = cell_id.split('-')[2]
-    showShadow(cellLocY, cellLocX, duckSize, "duck-placed");
-}
-
-function clearDuckShadow() {
-    for (let y = 0; y < 15; y++) { // 15x15 grid
-        for (let x = 0; x < 15; x++) {
-            const id = `cell-${y}-${x}`;
-            const cell = document.getElementById(id);
-            if(cell.classList.contains("duck-shadow")){
-                cell.classList.remove("duck-shadow");
+    draw() {
+        for (let y = 0; y < BOARD_SIZE; y++) { // 15x15 grid
+            for (let x = 0; x < BOARD_SIZE; x++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.id = getCellIDFromCoordinate(x, y); // Assigns a unique ID to each cell
+                this.element.appendChild(cell);
             }
         }
     }
-}
 
+    initializeDragEvents() {
+        const duckCells = Array.from(this.element.children);
 
-function showShadow(cellLocY, cellLocX, duckSize, shadowClass) {
-    areas = getDuckShadow(cellLocY, cellLocX, duckSize);
-    areas.forEach((coords) => {
-        const [y, x] = coords;
-        // if out of range clear shadow and exit
-        if(y<0 || y>= 15 || x<0 || x>=15) {
-            clearDuckShadow();
+        duckCells.forEach((cell) => {
+            cell.addEventListener('dragover', (e) => {
+                e.preventDefault();
+                this.clearDuckShadow();
+
+                if(this.isDuckPlacable(e)) {
+                    this.showShadow(e, "duck-shadow");
+                }
+            });
+            cell.addEventListener('drop', (e) => {
+                this.clearDuckShadow();
+                if(this.isDuckPlacable(e)) {
+                    this.dropDuck(e);
+                }
+            });
+            cell.addEventListener('dragend', () => {
+              this.clearDuckShadow();
+            });          
+        })
+    }
+
+    dropDuck(e) {
+        const cell = e.target;
+        const duckSize = globalDraggedDuckSizeTracker;
+        const draggedDuck = new Duck(duckSize);
+        draggedDuck.element.setAttribute('draggable', false);
+        draggedDuck.element.style.position = 'absolute';
+        draggedDuck.element.style.left = `${cell.getBoundingClientRect().left}px`;
+        draggedDuck.element.style.top = `${cell.getBoundingClientRect().top}px`;
+
+        this.element.appendChild(draggedDuck.element);
+
+        this.clearDuckShadow();
+        this.showShadow(e, "duck-placed");
+    }
+
+    isDuckPlacable(e) {
+        const cell_id = e.target.id;
+        const coordinate = getCoordinateFromCellID(cell_id);
+        
+        const size = globalDraggedDuckSizeTracker
+        const coordinates = this.getDuckCoordinates(coordinate.x, coordinate.y, size);
+     
+        return this.isDuckInBound(coordinates) && !this.isDuckOverLapping(coordinates);
+    }
+  
+    isDuckInBound(coordinates) {
+        for(let coord of coordinates) {
+              if(coord.x<0 || coord.x>= BOARD_SIZE || coord.y<0 || coord.y>=BOARD_SIZE) {
+                  return false;
+              }
+        }
+        return true;
+    }
+
+    isDuckOverLapping(coordinates) {
+        const cells = Array.from(this.element.children);
+        
+        for(let cell of cells) {
+          for(let coordinate of coordinates) {
+            const cellCoordinate = getCoordinateFromCellID(cell.id)
+            if(
+                cellCoordinate.x === coordinate.x &&
+                cellCoordinate.y === coordinate.y &&
+                cell.classList.contains('duck-placed')
+             ) return true; 
+          }
+        }
+        return false;
+    }
+  
+    getDuckCoordinates(refX, refY, size) {
+        const width = getComputedStyle(document.documentElement).getPropertyValue('--'+size+'-width').trim();
+        const length = getComputedStyle(document.documentElement).getPropertyValue('--'+size+'-height').trim();
+        const coordinates = [];
+        for(let w = 0; w < width; w++) {
+            for(let l = 0; l < length; l++) {
+                coordinates.push({
+                    x: refX + w,
+                    y: refY + l,
+                })
+            }
+        }
+        return coordinates;
+    }
+  
+    clearDuckShadow() {
+        const duckCells = Array.from(this.element.children);
+        duckCells.forEach(function (cell) {
+            if(cell.classList.contains("duck-shadow")){
+                cell.classList.remove("duck-shadow");
+            }
+        });
+    }
+
+    showShadow(e, shadowClass) {
+        const cell = e.target
+        const cell_id = cell.id;
+        const coordinate = getCoordinateFromCellID(cell_id);
+        
+        const size = globalDraggedDuckSizeTracker
+        const duckCoordinates = this.getDuckCoordinates(coordinate.x, coordinate.y, size);
+        
+        if(!(this.isDuckInBound(duckCoordinates) && !this.isDuckOverLapping(duckCoordinates))) {
             return;
         }
-        
-        const id = `cell-${y}-${x}`;
-        const cell = document.getElementById(id);
-        if(cell === null) console.log(id);
-        cell.classList.add(shadowClass);
-    })
+      
+        const cells = Array.from(this.element.children);
+        cells.forEach((cell) => {
+            duckCoordinates.forEach((duckCoordinate) => {
+                const cellCoordinate = getCoordinateFromCellID(cell.id)
+                if(
+                    cellCoordinate.x === duckCoordinate.x &&
+                    cellCoordinate.y === duckCoordinate.y
+                ) {
+                    cell.classList.add(shadowClass);
+                }
+            });
+        });
+    }
 }
 
-function getDuckShadow(refY, refX, size) {
-    const shadows = [];
 
-    const areas = duckSizeToArea[size];
-    areas.forEach((coords) => {
-        const [y, x] = coords;
-        const newY = parseInt(refY, 10) + parseInt(y, 10);
-        const newX = parseInt(refX, 10) + parseInt(x, 10);
-        shadows.push([newY,  newX]);
-    })
-    return shadows;
+class DuckTray {
+    constructor(duckTrayElement) {
+        this.element = duckTrayElement;
+    }
+
+    addDucks(ducks) {
+        for(const duck of ducks) {
+            this.element.appendChild(duck.element);
+        }
+    }
+
+    createNewDuckOnDrag() {
+        const ducks = Array.from(this.element.children);
+        ducks.forEach((img) => {
+            img.addEventListener('dragstart', (e) => {
+                globalDraggedDuckSizeTracker = getSizeFromImageElement(img)
+            });
+        })
+    }
 }
 
-function notInBound(areas) {
-    areas.forEach((coords) => {
-        const [y, x] = coords;
-        if(y<0 || y>= 15 || x<0 || x>=15) return true;
-        console.log("evaluate", y, x)
-    })
-    return false;
+
+class Arena {
+    constructor(board, duckTray) {
+        this.board = board;
+        this.duckTray = duckTray;
+    }
+
+    initialize() {
+        this.board.draw();
+        this.setUpDragAndDrop();
+    }
+
+    setUpDragAndDrop() {
+        this.duckTray.createNewDuckOnDrag();
+        this.board.initializeDragEvents();
+    }
 }
+
+
+// game setup
+const board = new Board(document.getElementById(`battleship-grid`));
+const duckTray = new DuckTray(document.getElementById(`ducks-container`));
+duckTray.addDucks(createDucksFromDuckSizes(DUCK_SIZES))
+
+const arena = new Arena(board, duckTray);
+
+arena.initialize();

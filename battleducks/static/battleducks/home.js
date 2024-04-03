8efe12bc7060ee177d157095a2ab8bd9594a60fa
AndrewTian12 <actian@andrew.cmu.edu>
2024-04-03 13:18:48 -0400

    Added Game Creation. Adjusted Models to better handle Game creation.

---
+++ battleducks/static/battleducks/home.js
@@ -0,0 +1,63 @@
+function createRoom() {
+    $.ajax({
+        url: "/battleducks/create-room",
+        type: "POST",
+        data: `csrfmiddlewaretoken=${getCSRFToken()}`,
+        dataType : "json",
+        success: openRoom,
+        error: updateError
+    });
+}
+
+function openRoom(response) {
+    let room_code = response.room_code;
+    let lastIndex = room_url.lastIndexOf('/');
+    window.location.pathname = room_url.substring(0, lastIndex + 1) + room_code;
+}
+
+function updateError(xhr) {
+    if (xhr.status === 0) {
+        displayError("Cannot connect to server")
+        return
+    }
+
+    if (!xhr.getResponseHeader('content-type') === 'application/json') {
+        displayError("Received status=" + xhr.status)
+        return
+    }
+
+    let response = JSON.parse(xhr.responseText)
+    if (response.hasOwnProperty('error')) {
+        displayError(response.error)
+        return
+    }
+
+    displayError(response)
+}
+
+function displayError(message) {
+    $("#error").html(message);
+}
+
+function getCSRFToken() {
+    let cookies = document.cookie.split(";")
+    for (let i = 0; i < cookies.length; i++) {
+        let c = cookies[i].trim()
+        if (c.startsWith("csrftoken=")) {
+            return c.substring("csrftoken=".length, c.length)
+        }
+    }
+    return "unknown";
+}
+
+document.querySelector('#room-name-input').focus();
+document.querySelector('#room-name-input').onkeyup = function(e) {
+    if (e.key === 'Enter') {  // enter, return
+        document.querySelector('#room-name-submit').click();
+    }
+};
+
+document.querySelector('#room-name-submit').onclick = function(e) {
+    var roomName = document.querySelector('#room-name-input').value;
+    window.location.pathname = '/battleducks/room/' + roomName;
+};
--- battleducks/views.py
+++ battleducks/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+from battleducks.models import Game
+
+import json
 
 # Create your views here.
-
+ALLOWED_ROOM_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ROOM_CODE_LENGTH = 4
 
 @login_required
 def index(request):
@@ -16,6 +23,7 @@ def login(request):
 @login_required
 def room(request, room_name):
     return render(request, "battleducks/room.html", {"room_name": room_name})
+
 @login_required
 def duck_placement(request, room_name, user_id):
     context = {
@@ -24,3 +32,30 @@ def duck_placement(request, room_name, user_id):
     }
     return render(request, "battleducks/duck_placement.html", context)
 
+@login_required
+def create_room(request):
+    if request.method != 'POST':
+        return _my_json_error_response("You must use a POST request for this operation", status=405)
+
+    invalid_code = True
+    while (invalid_code):
+        room_code = get_random_string(length=ROOM_CODE_LENGTH, allowed_chars=ALLOWED_ROOM_CHARS)
+        invalid_code = Game.objects.filter(room_code = room_code).exists()
+
+    # Create new Game
+    game = Game(
+        player1 = request.user,
+        room_code = room_code,
+        creation_time = timezone.now()
+    )
+    game.save()
+
+    my_response = {'room_code': room_code}
+    response_json = json.dumps(my_response)
+
+    return HttpResponse(response_json, content_type='application/json')
+
+def _my_json_error_response(message, status=200):
+    # You can create your JSON by constructing the string representation yourself (or just use json.dumps)
+    response_json = '{"error": "' + message + '"}'
+    return HttpResponse(response_json, content_type='application/json', status=status)

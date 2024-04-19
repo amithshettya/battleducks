ee0f502e0b9b948a3fb15a7a8f2d84042eb1ba00
amith-shetty <amithshettya@gmail.com>
2024-04-18 22:02:24 -0400

    Interated game work flow - login, lobby and duck placement sync - save duck position in db

--- battleducks/urls.py
+++ battleducks/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("room/<str:room_name>", views.room, name="room"),
     path("room/<str:room_name>/place_ducks/<int:user_id>", views.place_ducks, name="place_ducks"),
+    path("room/<str:room_name>/save_ducks", views.save_ducks, name="save_ducks"),
     path("create-room", views.create_room, name="create-room"),
     path("leaderboard", views.leaderboard, name="leaderboard")
 ]
